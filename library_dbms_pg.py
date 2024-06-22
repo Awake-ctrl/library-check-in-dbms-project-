beee36725f390037dedcc8a7558a54c7848dbb69
Awake-ctrl <kallepallysaikiran21@gmail.com>
2024-06-22 23:03:17 +0530

    interface is updated

--- library_dbms_pg.py
+++ library_dbms_pg.py
@@ -100,13 +100,14 @@ class LibraryDBMS:
         self.people_count_label.grid(row=3, column=0, columnspan=2, pady=10)
 
         self.scan_label = tk.Label(self.library_frame, text="Scan Here", font=("Helvetica", 16))
+        
         self.scan_label.grid(row=4, column=0, columnspan=2, pady=10)
 
         self.scan_entry = tk.Entry(self.library_frame, font=("Helvetica", 16))
         self.scan_entry.grid(row=5, column=0, columnspan=2, pady=10)
 
         # Button to search the ID in the database
-        self.search_button = tk.Button(self.library_frame, text="Search ID", command=self.search_id, font=("Helvetica", 16))
+        self.search_button = tk.Button(self.library_frame, text="Search ID", command=lambda: self.search_id(library_name), font=("Helvetica", 16))
         self.search_button.grid(row=6, column=0, columnspan=2, pady=5)
 
         self.person_details_label = tk.Label(self.library_frame, text="", font=("Helvetica", 16))
@@ -119,7 +120,7 @@ class LibraryDBMS:
         self.current_time_label.config(text=f"Current Time: {current_time}")
         self.root.after(1000, self.update_time)
 
-    def search_id(self):
+    def search_id(self,library_name):
         person_id = self.scan_entry.get()
         conn = psycopg2.connect(
             dbname="library_dbms",
@@ -130,18 +131,25 @@ class LibraryDBMS:
         )
         cursor = conn.cursor()
 
-        cursor.execute('SELECT * FROM students WHERE id=%s', (person_id,))
+        cursor.execute('SELECT * FROM student WHERE id=%s', (person_id,))
         student = cursor.fetchone()
 
         cursor.execute('SELECT * FROM faculty WHERE id=%s', (person_id,))
         faculty = cursor.fetchone()
-
+        
         if student:
-            self.display_person_details(f"Name: {student[1]}, ID: {student[0]}, Branch: {student[2]}, Year: {student[3]}, UG/PG: {student[4]}")
-            self.log_entry(person_id, "student")
+            id = student[0]
+            name = student[1]
+            program = student[2]
+            department = student[3]
+            self.display_person_details(f"Name: {student[1]}, ID: {student[0]}, Program: {student[2]}, Department: {student[3]}")
+            self.log_entry(id, name, program, department, library_name,"student")
         elif faculty:
-            self.display_person_details(f"Name: {faculty[1]}, ID: {faculty[0]}, Department: {faculty[2]}, Streak of Days: {faculty[4]}")
-            self.log_entry(person_id, "faculty")
+            id = faculty[0]
+            name = faculty[1]
+            department = faculty[2]
+            self.display_person_details(f"Name: {faculty[1]}, ID: {faculty[0]}, Department: {faculty[2]}")
+            self.log_entry(id, name, None, department,library_name, "faculty")
         else:
             messagebox.showerror("Error", "ID not found")
 
@@ -164,7 +172,7 @@ class LibraryDBMS:
         self.scan_entry.grid(row=5, column=0, columnspan=2, pady=10)
         self.search_button.grid(row=6, column=0, columnspan=2, pady=5)
 
-    def log_entry(self, person_id, person_type):
+    def log_entry(self, id, name, program, department,library_name, person_type):
         conn = psycopg2.connect(
             dbname="library_dbms",
             user="postgres",
@@ -174,14 +182,14 @@ class LibraryDBMS:
         )
         cursor = conn.cursor()
 
-        cursor.execute('SELECT * FROM logs WHERE person_id=%s AND person_type=%s AND check_out_time IS NULL', (person_id, person_type))
+        cursor.execute('SELECT * FROM log WHERE id=%s AND type=%s AND checkout IS NULL', (id, person_type))
         log = cursor.fetchone()
 
         if log:
-            cursor.execute('UPDATE logs SET check_out_time=%s WHERE id=%s', (time.strftime('%Y-%m-%d %H:%M:%S'), log[0]))
+            cursor.execute('UPDATE log SET checkout=%s WHERE id=%s AND checkout IS NULL', (time.strftime('%H:%M:%S'), id))
             self.update_people_count(-1)
         else:
-            cursor.execute('INSERT INTO logs (person_id, person_type, check_in_time) VALUES (%s, %s, %s)', (person_id, person_type, time.strftime('%Y-%m-%d %H:%M:%S')))
+            cursor.execute('INSERT INTO log (id, name, type, program, department, date,library_name, checkin) VALUES (%s, %s, %s, %s, %s,%s, %s, %s)', (id, name, person_type, program, department, time.strftime('%Y-%m-%d'), library_name,time.strftime('%H:%M:%S')))
             self.update_people_count(1)
 
         conn.commit()
@@ -218,11 +226,11 @@ class LibraryDBMS:
         cursor = conn.cursor()
 
         if report_type == "date":
-            cursor.execute('SELECT * FROM logs ORDER BY check_in_time')
+            cursor.execute('SELECT * FROM log ORDER BY checkin')
         elif report_type == "category":
-            cursor.execute('SELECT person_type, COUNT(*) FROM logs GROUP BY person_type')
+            cursor.execute('SELECT type, COUNT(*) FROM log GROUP BY type')
         elif report_type == "user":
-            cursor.execute('SELECT person_id, COUNT(*) FROM logs GROUP BY person_id')
+            cursor.execute('SELECT id, COUNT(*) FROM log GROUP BY id')
 
         logs = cursor.fetchall()
 
@@ -291,3 +299,5 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = LibraryDBMS(root)
     root.mainloop()
+
+
